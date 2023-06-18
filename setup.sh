c0d2db0867f85f48a4b5becd34a95f64eb1a3a54
easyselfhost <easyselfhost@gmail.com>
2023-06-18 00:08:46 -0700

    Fixes sysctl command.

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ curl -fsSL https://get.docker.com | sudo sh
 curl -fsSL https://get.docker.com/rootless | sh
 
 # Setup port for non-root users
-sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80 >> /etc/sysctl.conf
+sudo sh -c "sysctl -w net.ipv4.ip_unprivileged_port_start=80 >> /etc/sysctl.conf"
