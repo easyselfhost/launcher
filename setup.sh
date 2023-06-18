d41932a7fa5b7e647c66c4cfdae2f28757f752eb
easyselfhost <easyselfhost@gmail.com>
2023-06-18 00:06:41 -0700

    Adds sudo to sysctl command.

--- setup.sh
+++ setup.sh
@@ -9,4 +9,4 @@ curl -fsSL https://get.docker.com | sudo sh
 curl -fsSL https://get.docker.com/rootless | sh
 
 # Setup port for non-root users
-sysctl -w net.ipv4.ip_unprivileged_port_start=80 >> /etc/sysctl.conf
+sudo sysctl -w net.ipv4.ip_unprivileged_port_start=80 >> /etc/sysctl.conf
